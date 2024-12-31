f966267cc5f16eb9e1ee6536af2db6ff8711bf71
Miguel <migueldoiche2@gmail.com>
2024-12-31 00:44:07 -0300

    Creating project

---
+++ src/main/java/com/miguelsd/project1spring/Project1springApplication.java
@@ -0,0 +1,13 @@
+package com.miguelsd.project1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguelsd/project1spring/Project1springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelsd.project1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
