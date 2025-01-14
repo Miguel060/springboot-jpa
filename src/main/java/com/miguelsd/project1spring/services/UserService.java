26e411c65db6d0467e01a333efdad6141c487676
Miguel <migueldoiche2@gmail.com>
2025-01-13 22:51:17 -0300

    Service layer, component registration

---
+++ src/main/java/com/miguelsd/project1spring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.miguelsd.project1spring.services;
+
+import com.miguelsd.project1spring.entities.User;
+import com.miguelsd.project1spring.repositores.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
